dd6c9744474393c002c9a108612b89ef5f9c9f77
SneyderHOL <1507@holbertonschool.com>
2020-09-27 19:27:01 +0000

    [UPDATE]: update script adding content of 1-hbnb.js file

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,4 +1,15 @@
 $(document).ready(function () {
+  var objs = {};
+  $('input').change(function () {
+    if ($(this).is(':checked')) {
+      objs[$(this).attr('data-name')] = $(this).attr('data-id');
+    } else if ($(this).is(':not(:checked)')) {
+      delete objs[$(this).attr('data-name')];
+    }
+    const names = Object.keys(objs);
+    $('.amenities h4').text(names.sort().join(', '));
+  });
+
   $.get('http://0.0.0.0:5001/api/v1/status', function (data) {
     console.log(data.status);
     if (data.status == 'OK') {
